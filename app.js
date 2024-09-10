2cc1aedbf69ed750855703a021f89e09574b6108
charles102030 <delarosacharles201@gmail.com>
2024-09-10 23:38:47 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+//AND
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log(x < y && z < y); //(x is less than y and y is less that z)
+console.log(x > y && y > z); //(both of the conditions are false)
+
+//OR
+let a = "hello";
+let b = "world";
+let c = "goodbye";
+
+console.log(a === b || a === c); // (neither condition is true)
+console.log(a === b || b === c);// (true, one condition is true)
+
+//NOT
+let isSunny = true;
+let isRainy = false;
+console.log(isSunny);
+console.log(isRainy);
+
+console.log(!(isSunny && isRainy));
+console.log(isSunny || isRainy);
